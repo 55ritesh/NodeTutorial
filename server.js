117c60fbe1b46d01319dae2c554c9361c8a29b07
Ritesh Kumar <ritesh02072002@gmail.com>
2024-02-25 01:13:21 +0530

    fs and os module notes

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("n");
+var fs = require("fs");
+var  os = require("os");
+
+
+var user = os.userInfo();
+console.log(user); 
+
+fs.appendFile('greeting.txt','Hi '+user.username+'!\n',()=>{
+    console.log('file is created');
+})

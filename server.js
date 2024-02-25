2e9d61f47df8d0affbcf9e32c0987e509ff78326
Ritesh Kumar <ritesh02072002@gmail.com>
2024-02-25 22:19:10 +0530

    more code added

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const Person = require('./models/person');
+const MenuItem = require('./models/MenuItem');
 
 app.get('/',function(req,res){
     res.send("Welocome to hotel ... How i can help you?");
@@ -44,6 +45,61 @@ app.get('/person',async(req,res)=>{
       console.log(error);
       res.status(500).json({error:'Internal Server Error'});
 
+    }
+})
+
+
+// Post Method to add a menuItem
+
+app.post('/menu',async (req,res)=>{
+    try{
+       
+        const data = req.body;
+        const newMenu = new MenuItem(data);
+        const response = await newMenu.save();
+
+        console.log('data saved');
+        res.status(200).json(response);
+
+    }
+    catch(err){
+        console.log(error);
+        res.status(500).json({error:'Internal Server Error'});
+    }
+})
+
+// GET METHOD to get Menu Item
+
+app.get('/menu',async (req,res)=>{
+    try{
+        const data = await MenuItem.find();
+        console.log('data fetched');
+        res.status(200).json(data);
+
+    }catch{
+          
+      console.log(error);
+      res.status(500).json({error:'Internal Server Error'});
+
+    }
+})
+
+app.get('/person/:workType',async (req,res)=>{
+    try{
+
+        const workType = req.params.workType;
+        if(workType=='chef' || workType=='manager' || workType == 'waiter'){
+
+            const response = await Person.find({work:workType});
+            console.log('response fetched');
+            res.status(200).json(response);
+        }else{
+            res.status(404).json({error:'Invalid work type'});
+        }
+
+    }catch(err){
+        console.log(error);
+      res.status(500).json({error:'Internal Server Error'});
     }
 })
 
